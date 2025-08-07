2245713e195f64c34e012b536829e009ea46cf0f
dev-youngwoong <k7y7o756@gmail.com>
2025-08-07 15:03:43 +0900

    0.0.0.3

--- SkillToolSystem/Assets/@Scripts/Utill/Skill/SkillData.cs
+++ SkillToolSystem/Assets/@Scripts/Utill/Skill/SkillData.cs
@@ -34,59 +34,30 @@ public enum SkillTargetLayer
 [CreateAssetMenu(fileName ="NewSkill", menuName = "Skills/SkillData")]
 public class SkillData : ScriptableObject
 {
-    [Header("스킬 타겟레이어")]
-    public SkillTargetLayer TargetLayer; 
-
-    [Header("스킬 정보")]
+    public SkillTargetLayer TargetLayer;
     public string skillName;
     public float coolTime;
     public float damage;
     public int hitCount;
     public Vector2 startSkillPoint;
     public Vector2 skillRange;
-
-    [Header("소모 자원")]
     public float hp;
     public float mp;
-
-    [Header("슈퍼아머")]
     public bool superArmor;
-
-    [Header("스킬 캐스팅")]
     public bool skillCasting;
     public float castingTime;
-
-    [Header("스킬 시전 중 조작")]
     public bool onSkillMove;
-
-    [Header("스킬 특수버프")]
     public SkillSpecialAbility specialAbility;
-
-    [Header("스킬 속성")]
     public SkillElement skillElement;
-
-    [Header("스킬 단일or다중 타겟")]
     public SkillHitType skillHitType;
-
-    [Header("스킬 범위 타입")]
-    public SkillRangeType skillRangeType;   
-
-    [Header("스킬 넉백정보")]
+    public SkillRangeType skillRangeType;
     public bool knockBack;
     public Vector2 knockBackForce;
-
-    [Header("스킬 사운드")]
     public AudioClip skillSFX;
-
-    [Header("스킬 이펙트")]
     public GameObject SkillEffect;
     public Vector2 effectPos;
-
-    [Header("타격 이펙트")]
     public GameObject HitEffect;
     public Vector2 hitEffectPos;
-
-    [Header("카메라 연출")]
     public bool isCameraShake;
     public float shakePower;
     public Vector2 shakeDir;
---
+++ SkillToolSystem/Assets/@Scripts/Utill/Skill/SkillDataCustomEditor.cs
@@ -0,0 +1,87 @@
+using UnityEditor;
+using UnityEngine;
+[CustomEditor(typeof(SkillData))]
+public class SkillDataCustomEditor : Editor
+{
+    private bool showBasic = true;
+    private bool showCost = true;
+    private bool showEffect = true;
+    private bool showCamera = true;
+    
+    public override void OnInspectorGUI()
+    {
+        SkillData skill = (SkillData)target;
+
+        serializedObject.Update();
+        EditorGUILayout.Space();
+        showBasic = EditorGUILayout.Foldout(showBasic, "기본 정보", true);
+        if (showBasic)
+        {
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("TargetLayer"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("skillName"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("coolTime"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("damage"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("hitCount"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("skillHitType"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("skillRangeType"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("skillElement"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("specialAbility"));
+        }
+
+        EditorGUILayout.Space();
+        showCost = EditorGUILayout.Foldout(showCost, "코스트 및 부가 효과", true);
+        if (showCost)
+        {
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("hp"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("mp"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("superArmor"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("knockBack"));
+            if (skill.knockBack)
+            {
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("knockBackForce"));
+            }
+        }
+
+        EditorGUILayout.Space();
+        showEffect = EditorGUILayout.Foldout(showEffect, "스킬 연출", true);
+        if (showEffect)
+        {
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("skillCasting"));
+            if (skill.skillCasting)
+            {
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("castingTime"));
+            }
+
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("onSkillMove"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("SkillEffect"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("effectPos"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("HitEffect"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("hitEffectPos"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("skillSFX"));
+            if (serializedObject.FindProperty("skillSFX") != null)
+            {
+
+            }
+        }
+
+        EditorGUILayout.Space();
+        showCamera = EditorGUILayout.Foldout(showCamera, "카메라 연출", true);
+        if (showCamera)
+        {
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("isCameraShake"));
+            if (skill.isCameraShake)
+            {
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("shakePower"));
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("shakeDir"));
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("shakeDuration"));
+            }
+        }
+
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("스킬 범위", EditorStyles.boldLabel);
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("startSkillPoint"));
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("skillRange"));
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
